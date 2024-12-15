a23d57ff1f15a1d38589b61c84bdecab16a8e966
Dominique MERY <dominique.mery@free.fr>
2024-12-15 19:06:46 +0100

    I commit

---
+++ labels.pl
@@ -0,0 +1,117 @@
+# LaTeX2HTML 2024.2 (Released October 21, 2024)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_abrial2010/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-b/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-eatcs/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-discont/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:documentation/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_labels{$key} = "$URL/" . q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-4-hrefhttp:m/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:past-exams-course/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_labels{$key} = "$URL/" . q|node41.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2024.2 (Released October 21, 2024)
+# labels from external_latex_labels array.
+
+
+$key = q/sec:course-discont/;
+$external_latex_labels{$key} = q|x16-430004 x16-430004:autorefsectionCourse DISCONT: Modelling hybrid systems|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$external_latex_labels{$key} = q|x4-30002 x4-30002:autorefsectionCourse MCFSI at Telecom Nancy|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$external_latex_labels{$key} = q|x10-200003 x10-200003:autorefsectionCourse MsC in Computer Science: Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:documentation/;
+$external_latex_labels{$key} = q|x17-440004.1 x17-440004.1:autorefsubsectionDocumentation|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$external_latex_labels{$key} = q|x13-380003.3.4 x13-380003.3.4:autorefsubsubsectionEvent-B Archives for the tutorial 3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_latex_labels{$key} = q|x13-280003.3 x13-280003.3:autorefsubsectionEvent-B Models|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$external_latex_labels{$key} = q|x5-80002.1.4 x5-80002.1.4:autorefsubsubsectionLecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>.|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:past-exams-course/;
+$external_latex_labels{$key} = q|x15-420003.5 x15-420003.5:autorefsubsectionPast exams of the Course MsC in Computer Science: Modelling and verifying software-based systems for Master in Comp|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_latex_labels{$key} = q|x14-410003.4 x14-410003.4:autorefsubsectionProject|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_latex_labels{$key} = q|x5-40002.1 x5-40002.1:autorefsubsectionSlides for the course MCFSI|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$external_latex_labels{$key} = q|x11-210003.1 x11-210003.1:autorefsubsectionSlides of the course|; 
+$noresave{$key} = "$nosave";
+
+1;
+
